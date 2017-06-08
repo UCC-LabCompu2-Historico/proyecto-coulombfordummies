8e620721604a9000c8243c2120260f0d3deb0943
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-06-08 09:59:26 -0300

    se corrigio error en style de canvas y se agrego funcion draw_2arc

--- Funciones.js
+++ Funciones.js
@@ -1,4 +1,24 @@
 /**
  * Created by Alumno on 07/06/2017.
  */
+function draw_2arcs(){
+    var r = 60;
+    var x = 150;
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var ctx2 = c.getContext("2d");
 
+    ctx.lineWidth=8;
+    ctx.strokeStyle="blue";
+
+    ctx.beginPath();
+    ctx.arc(x, 300, r, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx2.lineWidth=8;
+    ctx2.strokeStyle="red";
+
+    ctx2.beginPath();
+    ctx2.arc(600-x, 300, r, 0, 2 * Math.PI);
+    ctx2.stroke();
+}
