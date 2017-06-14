8d4980726609cee3a5042a6191f80b031276c242
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-06-14 14:07:25 -0300

    se mejoro la funcion calcular

--- Funciones.js
+++ Funciones.js
@@ -1,48 +1,51 @@
 /**
  * Created by Alumno on 07/06/2017.
  */
-function draw_2arcs(){
+function draw_2arcs() {
     var r = 60;
     var x = 150;
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     var ctx2 = c.getContext("2d");
 
-    var color = calcular(color);
+    var color;
+    var color2;
 
-    ctx.lineWidth=8;
-    if(color==1){
-        ctx.strokeStyle="blue";
+
+    color =calcular("carga1")
+    ctx.lineWidth = 8;
+    if (color == 1) {
+        ctx.strokeStyle = "blue";
     }
-    if(color==0){
-        ctx.strokeStyle="red";
+    if (color == 0) {
+        ctx.strokeStyle = "red";
     }
 
     ctx.beginPath();
     ctx.arc(x, 300, r, 0, 2 * Math.PI);
     ctx.stroke();
 
-    var color2 = calcular(color2);
-
-    ctx2.lineWidth=8;
-    if(color2==1){
-        ctx2.strokeStyle="blue";
+    ctx2.lineWidth = 8;
+    if (color2 == 1) {
+        ctx2.strokeStyle = "blue";
     }
-    if(color2==0){
-        ctx2.strokeStyle="red";
+    if (color2 == 0) {
+        ctx2.strokeStyle = "red";
     }
 
     ctx2.beginPath();
-    ctx2.arc(600-x, 300, r, 0, 2 * Math.PI);
+    ctx2.arc(600 - x, 300, r, 0, 2 * Math.PI);
     ctx2.stroke();
 }
 
-function calcular(color,color2){
-    var c1 = document.getElementById("carga1").value;
-    var c2 = document.getElementById("carga2").value;
-    if(Number(c1)<0){color=1;}
-    else{color=0;}
-    if(Number(c2)<0){color2=1;}
-    else{color2=0;}
+function calcular(id) {
+    var d1;
+    var c1 = document.getElementById(id).value;
+    if (Number(c1) < 0) {
+        d1 = 1;
+    } else {
+        d1 = 0;
+    }
+    return d1;
 }
 
